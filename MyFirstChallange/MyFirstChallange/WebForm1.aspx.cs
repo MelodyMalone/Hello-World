2e205a2f5c1dae1415d682e2751ece98cf4db505
Alissa <alissadeneen@gmail.com>
2018-03-08 09:30:45 -0800

    MyFirstChallange
    The result from MyFirstChallange project.

---
+++ MyFirstChallange/MyFirstChallange/WebForm1.aspx.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace MyFirstChallange
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void resultButton_Click(object sender, EventArgs e)
+        {
+            string age = ageTextBox.Text;
+            string cash = cashTextBox.Text;
+
+            string result = "I would expect a " + age + " year old to have more than " + cash + " in their pocket.";
+
+            resultLabel.Text = result;
+        }
+    }
+}
